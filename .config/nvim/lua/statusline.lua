baecf9365c15fa4a0e26d0cba34d1fcd8c4111b0
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-07-09 23:21:40 +0800

    Finalize theme for statusline

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -2,35 +2,6 @@
 -- Author: lokesh-krishna
 -- MIT license, see LICENSE for more details.
 
--- stylua: ignore
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
-}
-
 require('lualine').setup {
   options = {
     theme = "auto",
@@ -42,7 +13,7 @@ require('lualine').setup {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
     lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
+    lualine_c = {},
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
