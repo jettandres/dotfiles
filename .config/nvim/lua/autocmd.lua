a4213090035989c71877c0e6ddcd3cf2acb61786
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-07-10 14:26:44 +0800

    Prettify autocmd

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,21 +1,21 @@
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
-  group = vim.api.nvim_create_augroup('treesitter_fold_workaround', {}),
-  callback = function()
-    vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
-  end
+	group = vim.api.nvim_create_augroup('treesitter_fold_workaround', {}),
+	callback = function()
+		vim.opt.foldmethod = 'expr'
+		vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
+	end
 })
 
 vim.api.nvim_create_autocmd('BufWritePost', {
-  group = vim.api.nvim_create_augroup('packer_nvim_auto_install_plugins', {}),
-  pattern = 'plugins.lua',
-  command = 'source <afile> | PackerSync'
+	group = vim.api.nvim_create_augroup('packer_nvim_auto_install_plugins', {}),
+	pattern = 'plugins.lua',
+	command = 'source <afile> | PackerSync'
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  group = vim.api.nvim_create_augroup('format_on_save', {}),
-  pattern = '*',
-  callback = function()
-    vim.lsp.buf.formatting_sync()
-  end
+	group = vim.api.nvim_create_augroup('format_on_save', {}),
+	pattern = '*',
+	callback = function()
+		vim.lsp.buf.formatting_sync()
+	end
 })
