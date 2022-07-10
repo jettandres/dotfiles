00e9f06e747aa66b94855a728b67cb75e3c173a5
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-07-10 11:07:49 +0800

    Fix linter errors

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,4 +1,4 @@
 local map = vim.api.nvim_set_keymap
 
-options = { noremap = true, silent = true }
+local options = { noremap = true, silent = true }
 map('n', '<C-n>', ':set hlsearch!<cr>', options)
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,37 +3,42 @@ local use = packer.use
 
 -- Automatically install and setup packer.nvim on any machine
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+    install_path })
 end
 
 return packer.startup(function()
-   use 'wbthomason/packer.nvim'
-   use {
- 	  'nvim-treesitter/nvim-treesitter',
-	   run = ':TSUpdate',
-   }
-   use 'nvim-treesitter/nvim-treesitter-context'
-   use 'andymass/vim-matchup'
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+  }
+  use 'nvim-treesitter/nvim-treesitter-context'
+  use 'andymass/vim-matchup'
 
-   -- lsp setup
-   use 'neovim/nvim-lspconfig'
-   use 'hrsh7th/nvim-cmp'
-   use 'hrsh7th/cmp-nvim-lsp'
-   use 'saadparwaiz1/cmp_luasnip'
-   use 'L3MON4D3/LuaSnip'
+  -- lsp setup
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip'
+  use {
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+  }
 
-   -- theme
-   use 'folke/tokyonight.nvim'
+  -- theme
+  use 'folke/tokyonight.nvim'
 
-   -- lualine
-   use {
-	   'nvim-lualine/lualine.nvim',
-	   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-   }
+  -- lualine
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 
   -- Automatically set up config after cloning packer.nvim
   if packer_bootstrap then
