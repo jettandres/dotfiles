8d62d89bcad710d21fd8952f5bdc9b1b13dd3ef1
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-07-11 00:49:17 +0800

    Setup auto-fix prettier errors on save
    This uses null-ls to apply prettier auto-fixes. Finally got this thing
    to work. Thanks to the creator of null-ls himself and his blog
    
    https://jose-elias-alvarez.medium.com/configuring-neovims-lsp-client-for-typescript-development-5789d58ea9c

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -52,33 +52,51 @@ local lspconfig = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
+local on_attach = function(client, bufnr)
+  if client.name == 'tsserver' then
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  else
+    -- Enable format on save capabilities
+    client.server_capabilities.documentFormattingProvider = true
+  end
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
+  vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, bufopts)
+  vim.keymap.set('n', ']e', vim.diagnostic.goto_next, bufopts)
+  vim.keymap.set('n', '<leader>E', vim.diagnostic.setloclist, bufopts)
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ac', vim.lsp.buf.code_action, bufopts)
+
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+    -- format on save
+    vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
+  end
+
+  if client.resolved_capabilities.document_range_formatting then
+    vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+  end
+
+end
+
 for _, server in ipairs(lsp_installer.get_installed_servers()) do
   lspconfig[server.name].setup {
-    on_attach = function(client, bufnr)
-      -- Enable format on save capabilities
-      client.server_capabilities.documentFormattingProvider = true
-      -- Enable completion triggered by <c-x><c-o>
-      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-      local bufopts = { noremap = true, silent = true, buffer = bufnr }
-
-      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
-      vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, bufopts)
-      vim.keymap.set('n', ']e', vim.diagnostic.goto_next, bufopts)
-      vim.keymap.set('n', '<leader>E', vim.diagnostic.setloclist, bufopts)
-
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-      vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-      vim.keymap.set('n', '<leader>ac', vim.lsp.buf.code_action, bufopts)
-    end,
+    on_attach = on_attach,
     capabilities = capabilities,
     settings = {
       Lua = {
@@ -98,3 +116,29 @@ for _, server in ipairs(lsp_installer.get_installed_servers()) do
     },
   }
 end
+
+-- null-ls for prettier
+local null_ls = require('null-ls')
+local prettier = require('prettier')
+
+null_ls.setup {
+  on_attach = on_attach
+}
+
+prettier.setup {
+  bin = 'prettierd',
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+}
