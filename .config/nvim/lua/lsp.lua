a0260c3d9a723da658c9dce0ec87e2662cdfc55f
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-07-22 13:36:35 +0800

    Install hadolint for docker dev

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -138,7 +138,8 @@ null_ls.setup {
   sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.formatting.prettierd
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.hadolint,
   },
 }
 
