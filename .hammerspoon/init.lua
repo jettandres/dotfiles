a5dab9b882d742bc51d6f6e7f3b746ef374cb2ed
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-08-10 19:05:09 +0800

    Use hyper key

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,9 +1,11 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function ()
-  hs.notify.new({title="Ola amigo", informativeText="Hello World"}):send()
+local hyper = { "cmd", "alt", "ctrl", "shift" }
+
+hs.hotkey.bind(hyper, "W", function()
+  hs.notify.new({ title = "Ola amigo", informativeText = "Hello World" }):send()
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function ()
+hs.hotkey.bind(hyper, "R", function()
   hs.reload()
 end)
 
-hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
+hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
