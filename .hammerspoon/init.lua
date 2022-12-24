59f50076fca234ea5c13e22274dd1339fa8b244c
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-12-24 19:33:07 +0800

    Remove window tiling manager from hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -9,13 +9,5 @@ end)
 hs.loadSpoon("SpoonInstall")
 
 spoon.SpoonInstall:andUse("EmmyLua")
-spoon.SpoonInstall:andUse("WindowGrid",
-  {
-    config = { gridGeometries = { { "6x4" } } },
-    hotkeys = { show_grid = { hyper, "g" } },
-    start = true,
-    disabled = true
-  }
-)
 
 hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
