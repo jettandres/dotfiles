d755e0916c9eb93071b715e8f1ce834eb1aeca43
Jett Robin Andres <jettrobin.andres@gmail.com>
2023-02-16 10:13:56 +0800

    Fix node_modules showing up in go to definition for lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,4 +1,4 @@
--- Manage lsp servers with nvim-lsp-installer
+-- Manage lsp servers with mason.nvim
 local lsp_installer = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 lsp_installer.setup {}
@@ -110,6 +110,26 @@ local on_attach = function(client, bufnr)
   end
 end
 
+-- util functions to filter node_modules during Go To Definition
+local function filter(arr, fn)
+  if type(arr) ~= "table" then
+    return arr
+  end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then
+      table.insert(filtered, v)
+    end
+  end
+
+  return filtered
+end
+
+local function filterReactDTS(value)
+  return string.match(value.uri, 'react/index.d.ts') == nil
+end
+
 mason_lspconfig.setup_handlers({
   function (server_name)
     lspconfig[server_name].setup {
@@ -138,6 +158,19 @@ mason_lspconfig.setup_handlers({
         },
       },
     },
+  }
+  end,
+  ["tsserver"] = function ()
+    lspconfig.tsserver.setup {
+      handlers = {
+        ['textDocument/definition'] = function(err, result, method, ...)
+          if vim.tbl_islist(result) and #result > 1 then
+            local filtered_result = filter(result, filterReactDTS)
+            return vim.lsp.handlers['textDocument/definition'](err, filtered_result, method, ...)
+          end
+          vim.lsp.handlers['textDocument/definition'](err, result, method, ...)
+        end
+      }
     }
   end
 })
