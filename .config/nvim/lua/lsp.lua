142681ec11f145a09505aecd91002d55caa2af56
Jett Robin Andres <jettrobin.andres@gmail.com>
2023-03-27 21:18:50 +0800

    restore fmt on save but only for go

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -115,9 +115,9 @@ local on_attach = function(client, bufnr)
     -- format on save
     vim.api.nvim_create_autocmd('BufWritePre', {
       group = vim.api.nvim_create_augroup('format_on_save', {}),
-      pattern = '*',
+      pattern = '*.go',
       callback = function()
-        --vim.lsp.buf.format()
+        vim.lsp.buf.format()
       end
     })
   end
