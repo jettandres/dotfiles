016a6064f52d3800301c07a808472588ece0a4d3
jettandres <jettrobin.andres@gmail.com>
2023-10-10 12:28:52 +0800

    Enable format on save only on Go files

--- .config/nvim/lua/lspsetup.lua
+++ .config/nvim/lua/lspsetup.lua
@@ -117,7 +117,7 @@ local on_attach = function(client, bufnr)
     -- format on save
     vim.api.nvim_create_autocmd('BufWritePre', {
       group = vim.api.nvim_create_augroup('format_on_save', {}),
-      --pattern = '*.go',
+      pattern = '*.go',
       callback = function()
         vim.lsp.buf.format()
       end
