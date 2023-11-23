a6aa4469dcc977455186f31710a4bd8341b1b41b
jettandres <jettrobin.andres@gmail.com>
2023-11-23 10:28:53 +0800

    Setup gopath to properly use installed go executables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set -gx EDITOR nvim
 set -gx ANDROID_HOME $HOME/Android/Sdk
 fish_add_path $ANDROID_HOME/emulator
 fish_add_path $ANDROID_HOME/platform-tools
+fish_add_path (go env GOPATH)/bin
 
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
