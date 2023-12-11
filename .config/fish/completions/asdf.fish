245488d6f4c92d1e7e1ce8748fe4198d2ba479ca
jettandres <jettrobin.andres@gmail.com>
2023-12-11 23:00:58 +0800

    Clean up unused config files

--- .config/fish/completions/asdf.fish
+++
@@ -1,1 +0,0 @@
-/home/jettandres/.asdf/completions/asdf.fish
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx EDITOR nvim
-
-set -gx ANDROID_HOME $HOME/Android/Sdk
-fish_add_path $ANDROID_HOME/emulator
-fish_add_path $ANDROID_HOME/platform-tools
-fish_add_path (go env GOPATH)/bin
-
-# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
-complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-source ~/.asdf/asdf.fish
+if status is-login
+    if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
+       #exec sway
+    end
+end
