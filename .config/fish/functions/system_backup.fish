35f0fc678ff39ffba4260f853107d67b2218028c
jettandres <jettrobin.andres@gmail.com>
2023-12-11 23:08:45 +0800

    Remove duplicate files

--- .config/fish/functions/system_backup.fish
+++
@@ -1,7 +0,0 @@
-function system_backup
-  echo "System backup initiated."
-  echo "Invoking rsync..."
-
-  sudo rsync -aAXHvSx -e ssh --delete --numeric-ids --info=progress2 --exclude-from rsync_backup.filter / jettandres@192.168.254.107:/media/lenovo-x230/backups/$(date +%Y-%m-%d:_%T)/
-  echo "System backup complete!"
-end
