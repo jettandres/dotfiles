7045da9793247b2289ad0b96a2ad3728b33a065e
jettandres <jettrobin.andres@gmail.com>
2023-12-16 14:42:36 +0800

    Add icons for volume up and down

--- .config/sway/scripts/controls/volume_up.sh
+++ .config/sway/scripts/controls/volume_up.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 10%+;
-wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}' | xargs -I {} notify-send -a "wpctl" -c "volume" -t 500 'Volume' '{}';
+wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}' | xargs -I {} notify-send -a "wpctl" -c "volume" -t 500 '󰝝 Volume' '{}';
