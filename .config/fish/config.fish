6fc603e8c8ab56301e5bf24f6d755b4fa44b918a
jettandres <jettrobin.andres@gmail.com>
2024-01-10 14:23:26 +0800

    Add aws cli autocomplete

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,6 @@ if status is-login
 end
 
 source /opt/asdf-vm/asdf.fish
+
+# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
