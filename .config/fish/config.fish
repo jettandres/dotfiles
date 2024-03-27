804a13af8df6dbbac889ba92bd4a12f069959f8a
jettandres <jettrobin.andres@gmail.com>
2024-03-27 21:29:47 +0800

    Add go path for installed go packages

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,5 +10,8 @@ end
 
 source /opt/asdf-vm/asdf.fish
 
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
