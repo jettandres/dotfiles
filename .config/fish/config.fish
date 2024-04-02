5716093bf1d4a32912afb4f01122b8b8fce45a22
jettandres <jettrobin.andres@gmail.com>
2024-04-02 14:21:53 +0800

    Add asdf setup for go

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,3 +12,6 @@ fish_add_path (go env GOPATH)/bin
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 source ~/.asdf/asdf.fish
+
+# Golang setup
+source ~/.asdf/plugins/golang/set-env.fish
