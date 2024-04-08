7fb9c4c1e3ae04582b398cba96d51315f70599ab
jettandres <jettrobin.andres@gmail.com>
2024-04-08 11:52:31 +0800

    Update neovim and plugins to latest version

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -15,7 +15,7 @@ return {
 
   { 'rcarriga/nvim-dap-ui',
   event = { "VeryLazy" },
-  dependencies = { 'mfussenegger/nvim-dap' },
+  dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
   config = function()
     require('dapui').setup({
       layouts = {
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,6 +1,6 @@
 return {
 	"folke/tokyonight.nvim",
 	lazy = false,
-	priority = 1000,
+	prioritty = 1000,
 	opts = {},
 }
