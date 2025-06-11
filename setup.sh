70fd08da1d8b2dc235d687a34342bd403e7bd58b
jettandres <jettrobin.andres@gmail.com>
2025-06-11 22:21:45 +0800

    Add restart after nix shell install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-# install nix-shell
-sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
-nix-shell ./nix-shell.nix
-chezmoi init --source .
+if [ -z "$NIX_ENV_READY" ]; then
+  echo "Installing Nix..."
+  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+
+  echo "Restarting shell..."
+  # Set a flag so we know we've already installed Nix next time
+  exec env NIX_ENV_READY=1 "$SHELL" -l
+else
+  echo "Shell restarted. Running nix-shell..."
+  nix-shell ./nix-shell.nix
+fi
