2030bd9a44ad08272fce546151af61f7e5e1767f
jettandres <jettrobin.andres@gmail.com>
2025-08-23 12:12:39 +0800

    Trim down autopairs for now

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -2,14 +2,7 @@ return {
     {
       'windwp/nvim-autopairs',
       event = "InsertEnter",
-      config = function ()
-        local npairs = require("nvim-autopairs")
-        npairs.setup({
-           check_ts = true,
-           fast_wrap = {},
-        })
-      end
-
+      config = true
     },
     { "windwp/nvim-ts-autotag" },
 }
