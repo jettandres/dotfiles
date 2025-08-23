eee339b231d8c1ab37ade779641383755f8960fb
jettandres <jettrobin.andres@gmail.com>
2025-08-23 08:59:28 +0800

    Set asdf golang latest version

--- run_onchange_20_setup-asdf.sh
+++ run_onchange_20_setup-asdf.sh
@@ -11,6 +11,8 @@ if command -v asdf >/dev/null 2>&1; then
   # Ensure Go plugin
   if ! asdf plugin list | grep -q '^golang$'; then
     asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+    asdf install golang latest
+    asdf global golang latest
   fi
 
   # Source Go env script for Fish (only if installed)
