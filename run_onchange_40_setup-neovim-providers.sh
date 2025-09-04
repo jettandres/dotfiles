c0b95854d6c63211564fc546f277588fa3a87a75
jettandres <jettrobin.andres@gmail.com>
2025-09-04 20:14:51 +0800

    Add step for installing requirements for VectoreCode

--- run_onchange_40_setup-neovim-providers.sh
+++ run_onchange_40_setup-neovim-providers.sh
@@ -4,3 +4,7 @@ set -u
 
 npm install -g neovim
 python3 -m pip install pynvim
+
+# install uv for VectoreCode AI plugin (untested, but needed later)
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv tool install "vectorcode<1.0.0"
