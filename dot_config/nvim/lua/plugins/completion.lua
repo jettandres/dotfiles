a646d938170a28c170412812e6890369bd14a265
jettandres <jettrobin.andres@gmail.com>
2025-10-31 16:18:14 +0800

    Remove minuet autocomplete

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -61,7 +61,7 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer', 'minuet' },
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
         providers = {
           thesaurus = {
             name = 'blink-cmp-words',
