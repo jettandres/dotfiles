efc60041ca01a5d16e13b7f10f7378c2369762d4
jettandres <jettrobin.andres@gmail.com>
2025-11-24 17:29:27 +0800

    Fix eslint setup for react projects

--- dot_config/nvim/lua/plugins/linting.lua
+++ dot_config/nvim/lua/plugins/linting.lua
@@ -1,19 +1,27 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
-    vim.env.ESLINT_D_PPID = vim.fn.getpid()
-    lint.linters_by_ft = {
-      javascript = { 'eslint_d' },
-      typescript = { 'eslint_d' },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-    }
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+      vim.env.ESLINT_D_PPID = vim.fn.getpid()
+      lint.linters_by_ft = {
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      }
 
-    -- Auto lint on save, insert leave, or buffer enter
-    vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "BufEnter" }, {
-      callback = function() lint.try_lint() end,
-    })
-  end
+      -- Auto lint on save, insert leave, or buffer enter
+      vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "BufEnter" }, {
+        callback = function() lint.try_lint() end,
+      })
+    end
+  },
+  {
+    'esmuellert/nvim-eslint',
+    config = function()
+      require('nvim-eslint').setup({})
+    end
+  }
 }
