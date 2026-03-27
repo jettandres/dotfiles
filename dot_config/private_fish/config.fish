6b4c593a19771e25fdb3f058c80627b210c9d477
jettandres <jettrobin.andres@gmail.com>
2026-03-28 01:35:56 +0800

    Remove asdf configuration from fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,20 +3,6 @@ if status is-interactive
     atuin init fish --disable-up-arrow | source
 end
 
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
 set -gx EDITOR nvim
 
 if test -d $HOME/Android/Sdk
@@ -31,12 +17,6 @@ end
 
 # Detect pip executables
 fish_add_path $HOME/.local/bin
-
-# Set Golang
-# source ~/.asdf/plugins/golang/set-env.fish
-
-# Set JAVA_HOME
-. ~/.asdf/plugins/java/set-java-home.fish
 
 # opencode
 fish_add_path /home/jettandres/.opencode/bin
