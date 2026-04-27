fa3b9d0a5e74f8a125cb316cba4a87d69d5ce927
jettandres <jettrobin.andres@gmail.com>
2026-04-27 09:39:22 +0800

    Add go binaries to path

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -18,6 +18,9 @@ end
 # Detect pip executables
 fish_add_path $HOME/.local/bin
 
+# Go binaries
+fish_add_path $HOME/go/bin
+
 # opencode
 if test -d /home/jettandres
   fish_add_path /home/jettandres/.opencode/bin
