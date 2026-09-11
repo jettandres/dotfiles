6fe54c183949fd5428fd0dba9174c1f76dd822fe
jettandres <jettrobin.andres@gmail.com>
2026-09-11 11:21:09 +0800

    Backup fix for old fish path issue

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,20 @@ if status is-interactive
     atuin init fish --disable-up-arrow | source
 end
 
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 set -gx EDITOR nvim
 
 if test -d $HOME/Android/Sdk
@@ -18,19 +32,11 @@ end
 # Detect pip executables
 fish_add_path $HOME/.local/bin
 
-# Go binaries
-fish_add_path $HOME/go/bin
+# Set Golang
+# source ~/.asdf/plugins/golang/set-env.fish
+
+# Set JAVA_HOME
+. ~/.asdf/plugins/java/set-java-home.fish
 
 # opencode
-if test -d /home/jettandres
-  fish_add_path /home/jettandres/.opencode/bin
-else if test -d /home/jett
-  fish_add_path /home/jett/.opencode/bin
-end
-
-# opencode
-fish_add_path /home/jett/.opencode/bin
-function opencode
-  set -lx AWS_REGION "ap-south-1"
-  command opencode $argv
-end
+fish_add_path /home/jettandres/.opencode/bin
